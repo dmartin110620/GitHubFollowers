4393c3eeb41b81cc25f6644f246d0e2ef1c22fb2
dmartin110620 <danielmafr@unisabana.edu.co>
2026-01-13 21:45:15 -0500

    Empty State for search tab added. SearchBar for followers added

---
+++ GitHubFollowers/Custom Views/VIews/GFEmptyStateView.swift
@@ -0,0 +1,55 @@
+//
+//  GFEmptyStateView.swift
+//  GitHubFollowers
+//
+//  Created by Daniel Felipe Martin Franco on 1/13/26.
+//
+
+import UIKit
+
+class GFEmptyStateView: UIView {
+    
+    let messageLabel = GFTitleLabel(textAlignment: .center, fontSize: 28)
+    let logoImageview = UIImageView()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    init(message: String) {
+        super.init(frame: .zero)
+        messageLabel.text = message
+        configure()
+    }
+    
+    
+    private func configure() {
+        addSubview(messageLabel)
+        addSubview(logoImageview)
+        
+        messageLabel.numberOfLines  = 3
+        messageLabel.textColor      = .secondaryLabel
+        
+        logoImageview.image         = UIImage(named: "empty-state-logo")
+        logoImageview.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            messageLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor, constant: -150),
+            messageLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 40),
+            messageLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -40),
+            messageLabel.heightAnchor.constraint(equalToConstant: 200),
+            
+            logoImageview.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 1.3),
+            logoImageview.heightAnchor.constraint(equalTo: self.widthAnchor, multiplier: 1.3),
+            logoImageview.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: 170),
+            logoImageview.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 40)
+        ])
+    }
+}
--- GitHubFollowers/Screens/FollowerListVC.swift
+++ GitHubFollowers/Screens/FollowerListVC.swift
@@ -11,6 +11,7 @@ class FollowerListVC: UIViewController {
 
     var username: String!
     var followers: [Follower] = []
+    var filteredFollowers: [Follower] = []
     var page = 1
     var hasMoreFollowers = true
     
@@ -26,6 +27,7 @@ class FollowerListVC: UIViewController {
         configureViewController()
         getFollowers(username: username, page: page)
         configureDataSource()
+        configureSearchController()
     }
     
     
@@ -50,15 +52,32 @@ class FollowerListVC: UIViewController {
     }
     
     
+    private func configureSearchController() {
+        let searchController                                    = UISearchController()
+        searchController.searchResultsUpdater                   = self
+        searchController.searchBar.delegate                     = self
+        searchController.searchBar.placeholder                  = "Search for a GitHub user"
+        searchController.obscuresBackgroundDuringPresentation   = false
+        navigationItem.searchController                         = searchController
+    }
+    
+    
     func getFollowers(username: String, page: Int) {
+        showLoadingView()
         NetworkManager.shared.getFollowers(for: username, page: page) { [weak self] result in
             guard let self = self else { return }
+            self.dismissLoadingView()
             
             switch result {
             case .success(let followers):
                 if followers.count < 100 { self.hasMoreFollowers = false }
                 self.followers.append(contentsOf: followers)
-                self.updateData()
+                if self.followers.isEmpty {
+                    let message = "This user has no followers. Go follow them 👨‍💻"
+                    DispatchQueue.main.async { self.showEmptyStateView(with: message, in: self.view) }
+                    return
+                }
+                self.updateData(on: self.followers)
                 
             case .failure(let error):
                 self.presentGFAlertOnMainThread(title: "Something Happened!", message: error.rawValue, buttonTitle: "OK")
@@ -76,7 +95,7 @@ class FollowerListVC: UIViewController {
     }
     
     
-    func updateData() {
+    func updateData(on followers: [Follower]) {
         var snapshot = NSDiffableDataSourceSnapshot<Section, Follower>()
         snapshot.appendSections([.main])
         snapshot.appendItems(followers)
@@ -98,6 +117,23 @@ extension FollowerListVC: UICollectionViewDelegate {
             page += 1
             getFollowers(username: username, page: page)
         }
+    }
+    
+}
+
+
+extension FollowerListVC: UISearchResultsUpdating, UISearchBarDelegate {
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let filter = searchController.searchBar.text, !filter.isEmpty else { return }
+        
+        filteredFollowers = followers.filter { $0.login.lowercased().contains(filter.lowercased()) }
+        updateData(on: filteredFollowers)
+    }
+    
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        updateData(on: followers)
     }
     
 }
