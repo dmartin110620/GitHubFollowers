1a52b1aec5e969aff3dad214cea3768f29877a9e
dmartin110620 <danielmafr@unisabana.edu.co>
2026-02-02 22:27:59 -0500

    Optimization - Screen Sizes& Constants

--- GitHubFollowers/Custom Views/VIews/GFEmptyStateView.swift
+++ GitHubFollowers/Custom Views/VIews/GFEmptyStateView.swift
@@ -40,8 +40,11 @@ class GFEmptyStateView: UIView {
         logoImageview.image         = UIImage(named: "empty-state-logo")
         logoImageview.translatesAutoresizingMaskIntoConstraints = false
         
+        let centerYAnchorConstant: CGFloat = DeviceType.isiPhoneSE2 ? -80 : -120
+        let bottomAncchorConstant: CGFloat = DeviceType.isiPhoneSE2 ? 100 : 40
+        
         NSLayoutConstraint.activate([
-            messageLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor, constant: -150),
+            messageLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor, constant: centerYAnchorConstant),
             messageLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 40),
             messageLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -40),
             messageLabel.heightAnchor.constraint(equalToConstant: 200),
@@ -49,7 +52,7 @@ class GFEmptyStateView: UIView {
             logoImageview.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 1.3),
             logoImageview.heightAnchor.constraint(equalTo: self.widthAnchor, multiplier: 1.3),
             logoImageview.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: 170),
-            logoImageview.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 40)
+            logoImageview.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: bottomAncchorConstant)
         ])
     }
 }
--- GitHubFollowers/Utilities/Constants.swift
+++ GitHubFollowers/Utilities/Constants.swift
@@ -5,7 +5,8 @@
 //  Created by Daniel Felipe Martin Franco on 1/23/26.
 //
 
-import Foundation
+import UIKit
+
 
 enum SFSymbols {
     static let location     = "location.fill"
@@ -13,4 +14,26 @@ enum SFSymbols {
     static let gists        = "doc.on.doc.fill"
     static let following    = "person.2.fill"
     static let followers    = "heart.fill"
+}
+
+
+enum Images {
+    static let ghLogo       = UIImage(named: "gh-logo")
+}
+
+
+enum ScreenSize {
+    static let width        = UIScreen.current?.bounds.width
+    static let height       = UIScreen.current?.bounds.height
+    static let maxLength    = max(ScreenSize.width!, ScreenSize.height!)
+    static let minLength    = min(ScreenSize.width!, ScreenSize.height!)
+}
+
+
+enum DeviceType {
+    static let idiom        = UIDevice.current.userInterfaceIdiom
+    static let scale        = UIScreen().scale
+    static let nativeScale  = UIScreen().nativeScale
+    
+    static let isiPhoneSE2  = idiom == .phone && ScreenSize.maxLength == 667
 }
